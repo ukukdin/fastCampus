484526f64da5d09e327e6ca941a9d2570b66d5c0
엄인국 <eum714211@gmail.com>
2024-10-08 22:12:01 +0900

    error handle

--- api/src/main/java/org/delivery/api/common/api/Result.java
+++ api/src/main/java/org/delivery/api/common/api/Result.java
@@ -42,6 +42,7 @@ public class Result {
                 .build();
     }
     //평균적으로 많이 사용하는 경우 ( 에러코드와 어떤 상황이다라는 description 넘겨주면 에러코드와 메세지 세팅하고 description 담아주는것)
+    //현재 원하는 값이 담기지 않아서 오류 수정을 해야됩니다.
     public static Result ERROR(ErrorCodeIfs errorCodeIfs, String description){
         return Result.builder()
                 .resultCode(errorCodeIfs.getErrorCode())
