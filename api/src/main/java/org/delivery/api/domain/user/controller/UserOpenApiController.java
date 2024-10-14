eb41e825d60c170e089583db5fef5ef9bdf6f794
엄인국 <eum714211@gmail.com>
2024-10-15 00:44:09 +0900

    login and register part

--- api/src/main/java/org/delivery/api/domain/user/controller/UserOpenApiController.java
+++ api/src/main/java/org/delivery/api/domain/user/controller/UserOpenApiController.java
@@ -3,6 +3,7 @@ package org.delivery.api.domain.user.controller;
 import lombok.RequiredArgsConstructor;
 import org.delivery.api.common.api.Api;
 import org.delivery.api.domain.user.business.UserBusiness;
+import org.delivery.api.domain.user.controller.model.UserLoginRequest;
 import org.delivery.api.domain.user.controller.model.UserRegisterRequest;
 import org.delivery.api.domain.user.controller.model.UserResponse;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -25,7 +26,14 @@ public class UserOpenApiController {
     @PostMapping("/register")
     public Api<UserResponse> register(@Valid @RequestBody Api<UserRegisterRequest> request){
 
+        var response = userBusiness.register(request.getBody());
+        return Api.OK(response);
+    }
 
-
+    //로그인
+    @PostMapping("/login")
+    public Api<UserResponse> login(@Valid @RequestBody Api<UserLoginRequest> request){
+        var response = userBusiness.login(request.getBody());
+        return Api.OK(response);
     }
 }
---
+++ api/src/main/java/org/delivery/api/domain/user/controller/model/UserLoginRequest.java
@@ -0,0 +1,20 @@
+package org.delivery.api.domain.user.controller.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.validation.constraints.NotBlank;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class UserLoginRequest {
+
+    @NotBlank
+    private String email;
+
+    @NotBlank
+    private String password;
+
+}
