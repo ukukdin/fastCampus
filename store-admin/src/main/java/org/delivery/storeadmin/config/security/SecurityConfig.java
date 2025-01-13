bc8f6bd6bf9d33dd589bcc529eae8f8614d58468
엄인국 <eum714211@gmail.com>
2025-01-13 20:52:40 +0900

    secuirity continusssss

---
+++ store-admin/src/main/java/org/delivery/storeadmin/config/security/SecurityConfig.java
@@ -0,0 +1,42 @@
+package org.delivery.storeadmin.config.security;
+
+import java.util.List;
+import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity //security 활성화
+public class SecurityConfig {
+
+  private List<String> SWAGGER = List.of(
+      "/swagger-ui.html",
+      "/swagger-ui/**",
+      "/v3/api-docs/**"
+  );
+  @Bean
+  public SecurityFilterChain filterChain(HttpSecurity httpSecurity) throws Exception{
+      httpSecurity
+          .csrf().disable()
+          .authorizeHttpRequests(it ->{
+              it
+                  .requestMatchers(
+                      PathRequest.toStaticResources().atCommonLocations()
+                  ).permitAll() //resource에 대해서는 모든 요청 허용
+                  //swagger 는 인증 없이 통과
+                  .mvcMatchers(
+                      SWAGGER.toArray(new String[0])
+                  ).permitAll()
+
+                  //그 외의 모든 요청은 인증 사용
+                  .anyRequest().authenticated();
+          })
+          .formLogin(Customizer.withDefaults());
+
+      return httpSecurity.build();
+  }
+}
